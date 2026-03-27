902074318fee5beb5256312902bc3e09269695d3
Shiqiu17 <chen.shiqi@northeastern.edu>
2026-03-27 13:25:14 -0700

    Add more weather data

--- app/src/main/java/edu/northeastern/wea26sp_team3/WeatherItem.java
+++ app/src/main/java/edu/northeastern/wea26sp_team3/WeatherItem.java
@@ -5,11 +5,20 @@ public class WeatherItem {
     public final double temperature;
     public final int weatherCode;
     public final double windSpeed;
+    public final double humidity;
+    public final double feelsLike;
 
-    public WeatherItem(String time, double temperature, int weatherCode, double windSpeed) {
+    public WeatherItem(String time,
+                       double temperature,
+                       int weatherCode,
+                       double windSpeed,
+                       double humidity,
+                       double feelsLike) {
         this.time = time;
         this.temperature = temperature;
         this.weatherCode = weatherCode;
         this.windSpeed = windSpeed;
+        this.humidity = humidity;
+        this.feelsLike = feelsLike;
     }
 }
